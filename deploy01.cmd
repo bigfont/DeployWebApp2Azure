f9cdb35807d698c51502f7d76abaf976a41cc073
Shaun Luttin <shaunluttin@gmail.com>
2015-05-21 14:37:47 -0700

    Experiment with @

--- deploy01.cmd
+++ deploy01.cmd
@@ -1,1 +1,8 @@
 echo "My Custom Deployment Script"
+
+:: The @ supresses echoing of a line.
+:: THE %% accesses environmental variables.
+
+echo %SCM_TRACE_LEVEL%
+
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
--- deploy02.cmd
+++
@@ -1,3 +0,0 @@
-echo "My Custom Deployment Script"
-
-if "%SCM_TRACE_LEVEL%" NEQ "4" echo off
